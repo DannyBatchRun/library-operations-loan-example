ed29e9ef0d7734e28b1260350fd255cc4e11a7e5
DannyBatchRun <danielpeter.mnz@gmail.com>
2024-03-12 09:53:38 +0100

    Added credentials to database CockroachDB with Encryption Key maded by Jasypt

--- library-database-books-example/src/main/java/com/library/example/first/LibraryExampleFirstApplication.java
+++ library-database-books-example/src/main/java/com/library/example/first/LibraryExampleFirstApplication.java
@@ -1,9 +1,11 @@
 package com.library.example.first;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class LibraryExampleFirstApplication {
 
 	public static void main(String[] args) {
